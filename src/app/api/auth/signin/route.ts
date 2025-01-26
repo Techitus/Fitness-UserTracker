116fb697ba28ac930d1948dd27defbcbce5009f5
Kamal Paudel <techitus07@gmail.com>
2025-01-27 00:07:33 +0545

    signin and logout

---
+++ src/app/api/auth/signin/route.ts
@@ -0,0 +1,41 @@
+import { database } from "@/database/database";
+import { auth } from "@/database/schemas";
+import { eq } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+dotenv.config({ path: '.env.local' });
+export async function POST(req:NextRequest){
+    try{
+        const reqBody = await req.json()
+        const {email,password,confirmPassword} = reqBody
+
+        const [user]= await database.select().from(auth).where(eq(auth.email, email)).limit(1)
+
+        if(!user){
+            return NextResponse.json({error: "User not found"}, {status: 404})
+        }
+   const validPassword =  await bcrypt.compare(password, user.password)
+
+   const validConfirmPassword = await bcrypt.compare(confirmPassword, user.confirmPassword)
+     
+   if(!validPassword && !validConfirmPassword){
+    return NextResponse.json({error: "Please check your credentials..."}, {status: 404})
+   }
+     const tokenPayload = {
+        id : user.id,
+        username : user.username,
+        email : user.email
+     }
+  const token =  jwt.sign(tokenPayload, process.env.TOKEN_SECRET!, { expiresIn: '1h' })
+
+ const response =   NextResponse.json({message : "User logged in successfully",sucess : true}, {status: 200})
+    response.cookies.set("token",token,{
+        httpOnly : true
+    })
+     return response
+    }catch(error){
+        return NextResponse.json({error}, {status: 500})
+    }
+}
